9acd30281e6bc8e7c68e84bbc4f7cb35387b5a41
gersongerardcruz <ggerardcruz@gmail.com>
2023-03-07 20:50:13 +0800

    add preprocessing code and update data collection

--- src/backend/get_data.py
+++ src/backend/get_data.py
@@ -3,17 +3,29 @@ from openmeteo_py import Hourly, Options, OWmanager
 from datetime import datetime
 
 def get_weather_data(latitude, longitude, past_days, timezone):
+    """
+    Retrieves weather data from OpenWeather API and returns a pandas dataframe with relevant columns.
+
+    Args:
+    - latitude: float - The latitude of the location you want to retrieve data for
+    - longitude: float - The longitude of the location you want to retrieve data for
+    - past_days: int - The number of days of past data you want to retrieve
+    - timezone: str - The timezone of the location you want to retrieve data for
+
+    Returns:
+    - pandas dataframe - A dataframe with the relevant weather data columns
+    """
+    # Set up API options and download data
     hourly = Hourly()
     options = Options(latitude, longitude, past_days=past_days, timezone=timezone)
-
     mgr = OWmanager(options, hourly.all())
-
-    # Download data and convert to pandas df
     meteo = mgr.get_data()
+
+    # Convert to pandas dataframe
     df = pd.DataFrame(meteo['hourly'])
 
     # Get only data equal to or less than current time
-    current_time = end_time = datetime.now().strftime('%Y-%m-%dT%H:%M')
+    current_time = datetime.now().strftime('%Y-%m-%dT%H:%M')
     df = df[df["time"] <= current_time]
 
     # Define columns to retain for the model to predict temperature
---
+++ src/backend/preprocess.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import sys
+import os
+
+# Add the src directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils import *
+
+def create_lag_window(df, num_lags=3, delay=1):
+    """
+    Adds lag and rolling window columns to the input dataframe.
+    
+    Parameters:
+        df (pandas.DataFrame): input dataframe
+        num_lags (int): number of lagged columns to create (default: 3)
+        delay (int): time delay for lagged columns (default: 1)
+        
+    Returns:
+        pandas.DataFrame: dataframe with added lag and rolling window columns
+    """
+    # Set the time column as the index
+    df.set_index("time", inplace=True)
+    df = remove_id_column(df)
+
+    for column in df:
+        for lag in range(1,num_lags+1):
+            df[column + '_lag' + str(lag)] = df[column].shift(lag*-1-(delay-1))
+            df[column + '_avg_window_length' + str(lag+1)] = df[column].shift(-1-(delay-1)).rolling(window=lag+1,center=False).mean().shift(1-(lag+1))
+
+    df.dropna(inplace=True) 
+
+    mask = (df.columns.str.contains('apparent_temperature') | df.columns.str.contains('lag') | df.columns.str.contains('window'))
+    df_processed = df[df.columns[mask]]
+    
+    return df_processed
+
+# Load the data into a dataframe, preprocess, and save
+df = pd.read_csv("data/raw/train.csv")
+df_processed = create_lag_window(df)
+df_processed.to_csv("data/processed/train.csv")
