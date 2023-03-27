89798f9133814d6435672a2fcfa8c6e3d0f53882
gersongerardcruz <ggerardcruz@gmail.com>
2023-03-27 11:57:59 +0800

    add preprocessing dag

--- ec2_airflow/dag/utils.py
+++ ec2_airflow/dag/utils.py
@@ -3,6 +3,8 @@ import boto3
 import io 
 from openmeteo_py import Hourly, Options, OWmanager
 from datetime import datetime
+from airflow.hooks.S3_hook import S3Hook
+from io import StringIO
 
 def get_weather_data(filename, bucket_name, latitude=14.5995, longitude=120.9842, past_days=2, timezone="Asia/Shanghai"):
     """
@@ -50,6 +52,74 @@ def get_weather_data(filename, bucket_name, latitude=14.5995, longitude=120.9842
 
     bucket_name = bucket_name
     key = f"{filename}.csv"
+
+    # Write data to a file on S3
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+
+    s3.put_object(Body=csv_buffer.getvalue(), Bucket=bucket_name, Key=key)
+
+def remove_id_column(df):
+    """
+    Removes the ID column from a pandas dataframe.
+    Takes into account different spellings of "ID" and "Unnamed: 0".
+    Parameters:
+        df (pandas.DataFrame): The input dataframe.
+    Returns:
+        pandas.DataFrame: The dataframe with the ID column removed.
+    """
+    id_columns = ['ID', 'Id', 'id', 'Unnamed: 0']
+    for col in id_columns:
+        if col in df.columns:
+            return df.drop(columns=[col])
+    return df
+
+def create_lag_window(df, num_lags=3, delay=1):
+    """
+    Adds lag and rolling window columns to the input dataframe.
+    
+    Parameters:
+        df (pandas.DataFrame): input dataframe
+        num_lags (int): number of lagged columns to create (default: 3)
+        delay (int): time delay for lagged columns (default: 1)
+        
+    Returns:
+        pandas.DataFrame: dataframe with added lag and rolling window columns
+    """
+    # Set the time column as the index
+    df.set_index("time", inplace=True)
+    df = remove_id_column(df)
+
+    for column in df:
+        for lag in range(1,num_lags+1):
+            df[column + '_lag' + str(lag)] = df[column].shift(lag*-1-(delay-1))
+            df[column + '_avg_window_length' + str(lag+1)] = df[column].shift(-1-(delay-1)).rolling(window=lag+1,center=False).mean().shift(1-(lag+1))
+
+    df.dropna(inplace=True) 
+
+    mask = (df.columns.str.contains('apparent_temperature') | df.columns.str.contains('lag') | df.columns.str.contains('window'))
+    df_processed = df[df.columns[mask]]
+    
+    return df_processed
+
+def preprocess_data(filename, bucket_name):
+    # Connect to S3
+    s3 = boto3.client('s3')
+
+    filename = f"{filename}.csv"
+
+    # Get the weather data file from S3
+    obj = s3.get_object(Bucket=bucket_name, Key=filename)
+
+    # Load the weather data into a Pandas DataFrame
+    df = pd.read_csv(StringIO(obj['Body'].read().decode('utf-8')))
+    df = remove_id_column(df)
+
+    df_processed = create_lag_window(df)
+    df_processed = df_processed.reset_index(drop=True)
+
+    bucket_name = bucket_name
+    key = f"{filename}_processed.csv"
 
     # Write data to a file on S3
     csv_buffer = io.StringIO()
--- ec2_airflow/dag/weather_dag.py
+++ ec2_airflow/dag/weather_dag.py
@@ -5,14 +5,11 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
-from utils import *
-
-# Get the current timestamp in UTC
-timestamp = datetime.utcnow().strftime('%Y%m%dT%H%M%S')
+from utils import get_weather_data, preprocess_data
 
 # Construct the default_var using the timestamp
-default_filename = f'airflow_train_{timestamp}'
-default_bucket_name = "trial-gerson"
+default_filename = f'airflow_train'
+default_bucket_name = "airflow-gerson"
 
 # Load configuration from the Trigger DAG with Config option
 config = Variable.get("config", default_var={}, deserialize_json=True)
@@ -29,6 +26,10 @@ def run_get_data(**op_kwargs):
     # Call the get_weather_data function with the filename and bucket_name argument
     get_weather_data(filename=filename, bucket_name=bucket_name)
 
+def process_data(**op_kwargs):
+    # Call the preprocess data function with the filename and bucket_name argument
+    preprocess_data(filename=filename, bucket_name=bucket_name)
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -36,14 +37,14 @@ default_args = {
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'retries': 0
 }
 
 dag = DAG(
     'collect_data_dag',
     default_args=default_args,
-    description='Collecting historical weather data'
+    description='Collecting historical weather data',
+    schedule_interval=None
 )
 
 run_get_data = PythonOperator(
@@ -57,4 +58,12 @@ run_get_data = PythonOperator(
     dag=dag
 )
 
-run_get_data
+process_data = PythonOperator(
+    task_id='preprocess_data',
+    python_callable=process_data,
+    provide_context=True,
+    dag=dag
+)
+
+# Create dependency to ensure run_get_data runs first before process_data
+run_get_data >> process_data
