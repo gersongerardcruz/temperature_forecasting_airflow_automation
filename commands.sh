2ba4479a32e801e7dfddc5410b5c02500211bf20
gersongerardcruz <ggerardcruz@gmail.com>
2023-04-11 14:55:04 +0800

    add linux commands for project requirements

---
+++ commands.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+yes | sudo apt install python3-pip
+yes | sudo pip install apache-airflow
+yes | sudo pip install pandas 
+yes | sudo pip install s3fs
+yes | sudo pip install openmeteo-py
+yes | sudo pip install boto3
+yes | sudo apt  install awscli
+yes | sudo pip install apache-airflow-providers-amazon
+yes | sudo pip install joblib
+yes | sudo pip install scikit-learn
+yes | sudo pip install xgboost
